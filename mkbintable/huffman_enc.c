cda445a1b39934bcd953250dee30c870a1180e92
Martin Wolters <sirdzstic@gmail.com>
2012-11-11 14:39:38 +0100

    Unneeded headers

--- mkbintable/huffman_enc.c
+++ mkbintable/huffman_enc.c
@@ -10,7 +10,6 @@
  */
 
 #include <stdlib.h>
-#include <string.h>
 
 #include "..\shared\types.h"
 #include "huffman_enc.h"
--- mkbintable/main.c
+++ mkbintable/main.c
@@ -10,8 +10,6 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <Windows.h>
 
 #include "..\shared\types.h"
